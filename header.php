d286f4912d2ad9bd5fcd29ec87f001fb4446e48a
lamquang4 <113229340+lamquang4@users.noreply.github.com>
2024-05-11 20:05:58 +0700

    ewew

--- header.php
+++ header.php
@@ -35,11 +35,15 @@ if(isset($_GET['action']) && isset($_GET['id'])){
           <li id="menu"><a href="contact.php" class="choose"><span>Contact</span></a></li>
     
       <div class="group" id="search">
-      <svg fill="#000000" width="20px" height="20px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg" style="cursor: pointer;">
+      <form method="GET" action="shop.php#product11">
+        <button type="submit">
+        <svg fill="#000000" width="20px" height="20px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg" style="cursor: pointer;">
       <path d="M790.588 1468.235c-373.722 0-677.647-303.924-677.647-677.647 0-373.722 303.925-677.647 677.647-677.647 373.723 0 677.647 303.925 677.647 677.647 0 373.723-303.924 677.647-677.647 677.647Zm596.781-160.715c120.396-138.692 193.807-319.285 193.807-516.932C1581.176 354.748 1226.428 0 790.588 0S0 354.748 0 790.588s354.748 790.588 790.588 790.588c197.647 0 378.24-73.411 516.932-193.807l516.028 516.142 79.963-79.963-516.142-516.028Z" fill-rule="evenodd"></path>
   </svg>
-  <form method="GET" action="shop.php#product11">
-    <input id="search-item" type="search" placeholder="Search" name="text" class="input" tabindex="1">
+        </button>
+ 
+
+    <input id="search-item" type="text" placeholder="Search" name="text" class="input" tabindex="1">
    
 </form>
   </div> 
