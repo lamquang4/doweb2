6cb1c889e39aa48102e9db7c1cee0749e61287ce
lamquang4 <113229340+lamquang4@users.noreply.github.com>
2024-05-20 11:38:18 +0700

    yygy

--- user.php
+++ user.php
@@ -19,7 +19,7 @@ exit();
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=0.9,maximum-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1">
     <title>Profile</title>
 
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
